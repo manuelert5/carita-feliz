461f1613f3598ac9d9b061ab62be4ff0c8406e11
manuelert5 <32697344+manuelert5@users.noreply.github.com>
2017-10-11 02:29:12 -0600

    nnod

--- build/web/buscanotas.php
+++ build/web/buscanotas.php
@@ -1,7 +1,7 @@
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <?php
 include ("bd.php");
-oihiuhyiuhiuhiujhjio
+hola papu
 $caso=0;
 if ($_POST['codigomineduc']!=""){
     $caso = 1; // el usuario ingreso grado y mineduc
